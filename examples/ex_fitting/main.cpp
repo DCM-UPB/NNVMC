ace0851403b8879398caa17e0fb300a43c2daae8
Jan Kessler <jakessle@t-online.de>
2019-07-04 23:20:18 +0200

    added basic nnwf pre-fit example

--- examples/ex_fitting/main.cpp
+++ examples/ex_fitting/main.cpp
@@ -6,7 +6,6 @@
 
 #include "vmc/VMC.hpp"
 #include "vmc/MPIVMC.hpp"
-#include "vmc/EnergyMinimization.hpp"
 #include "nfm/Adam.hpp"
 #include "nfm/LogManager.hpp"
 #include "nnvmc/ANNWaveFunction.hpp"
@@ -24,7 +23,7 @@ int main()
     MPIVMC::Init(); // to avoid error with MPI-compiled VMC library
 
 
-    // --- Setup the ANN
+    // --- Setup and fit the ANN
 
     // Setup TemplNet
     constexpr auto dconf = DerivConfig::D12_VD1; // enable necessary derivatives
@@ -42,13 +41,43 @@ int main()
     random_device rdev;
     mt19937_64 rgen;
     rgen = mt19937_64(rdev());
-    auto rd = normal_distribution<double>(0, 0.1); // sigma 0.1
+    auto rd = normal_distribution<double>(0, sqrt(1./HIDDENLAYERSIZE));
 
     double vp[ann.getNVariationalParameters()];
-    for (auto & v : vp) { v = rd(rgen); }
+    for (auto &v : vp) { v = rd(rgen); }
     ann.setVariationalParameters(vp);
 
-    // --- VMC optimization of the NNWF
+    cout << endl << " - - - NNWF FITTING - - - " << endl << endl;
+
+    // generate some gaussian data for fit
+    const int ndata = 2000;
+    rd = normal_distribution<double>(0, 2.); // sigma 2.
+    double xdata[ndata], ydata[ndata];
+
+    for (int i = 0; i < ndata; ++i) {
+        xdata[i] = rd(rgen);
+        ydata[i] = exp(-0.5*xdata[i]*xdata[i]);
+        cout << "y( " << xdata[i] << " ) = " << ydata[i] << endl;
+    }
+
+    // setup fitting
+    NNFitCost fit_cost(&ann, ndata, xdata, ydata);
+
+    nfm::Adam adam(ann.getNVariationalParameters());
+    adam.setAlpha(0.01);
+    adam.setBeta1(0.75);
+    adam.setBeta2(0.95);
+    adam.setMaxNConstValues(20);
+    adam.setMaxNIterations(500);
+    adam.findMin(fit_cost, vp);
+
+    for (double x = -5.; x <= 5.; x += 0.5) {
+        double xv[1]{x};
+        ann.evaluate(xv);
+        cout << "P(" << x << ") = " << ann.getOutput(0) << endl;
+    }
+
+    // --- Compute VMC energy of the fitted NNWF
 
     // Declare the trial wave functions
     ANNWaveFunction<QTemplWrapper<NetType>> psi(1, 1, ann);
@@ -59,52 +88,23 @@ int main()
     HarmonicOscillator1D1P ham(w1);
 
 
-    cout << endl << " - - - FFNN-WF FUNCTION OPTIMIZATION - - - " << endl << endl;
+    cout << endl << " - - - NNWF ENERGY COMPUTATION - - - " << endl << endl;
 
     using namespace vmc;
     const long E_NMC = 100000l; // MC samplings to use for computing the initial/final energy
-    const long G_NMC = 20000l; // MC samplings to use for computing the energy and gradient
     double energy[4]; // energy
     double d_energy[4]; // energy error bar
 
     cout << "-> ham1:    w = " << w1 << endl << endl;
     VMC vmc(psi, ham); // VMC object used for energy/optimization
 
-    // set an integration range, because the NN might be completely delocalized
-    vmc.getMCI().setIRange(-7.5, 7.5);
 
-    // set fixed number of decorrelation steps
-    vmc.getMCI().setNdecorrelationSteps(1000);
-
-
-    cout << "   Starting energy:" << endl;
+    cout << "   Resulting energy:" << endl;
     vmc.computeEnergy(E_NMC, energy, d_energy);
     cout << "       Total Energy        = " << energy[0] << " +- " << d_energy[0] << endl;
     cout << "       Potential Energy    = " << energy[1] << " +- " << d_energy[1] << endl;
     cout << "       Kinetic (PB) Energy = " << energy[2] << " +- " << d_energy[2] << endl;
     cout << "       Kinetic (JF) Energy = " << energy[3] << " +- " << d_energy[3] << endl << endl;
-
-
-    // -- Setup Adam optimization
-    nfm::Adam adam(psi.getNVP(), true /* use averaging to obtian final result */);
-    adam.setAlpha(0.015);
-    adam.setBeta1(0.5);
-    adam.setBeta2(0.9);
-    adam.setMaxNConstValues(20); // stop after 10 constant values (within error)
-    adam.setMaxNIterations(200); // limit to 200 iterations
-    nfm::LogManager::setLoggingOn(); // normal logging
-
-
-    cout << "   Optimization . . ." << endl;
-    minimizeEnergy<EnergyGradientTargetFunction>(vmc, adam, E_NMC, G_NMC);
-    cout << "   . . . Done!" << endl << endl;
-
-    cout << "   Optimized energy:" << endl;
-    vmc.computeEnergy(E_NMC, energy, d_energy);
-    cout << "       Total Energy        = " << energy[0] << " +- " << d_energy[0] << endl;
-    cout << "       Potential Energy    = " << energy[1] << " +- " << d_energy[1] << endl;
-    cout << "       Kinetic (PB) Energy = " << energy[2] << " +- " << d_energy[2] << endl;
-    cout << "       Kinetic (JF) Energy = " << energy[3] << " +- " << d_energy[3] << endl << endl << endl;
 
 
     MPIVMC::Finalize();
